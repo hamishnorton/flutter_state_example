30506b17e45efeb825dbace6cbac88752f407404
Hamish Norton <hamish.norton@theta.co.nz>
2022-03-10 20:49:22 +1300

    create variable in main for setting the stateSystem to use.

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,73 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_state_example/cubit/cubit_state_screen.dart';
+import 'package:flutter_state_example/cubit/form_cubit.dart';
+import 'package:flutter_state_example/form_state/form_state_screen.dart';
+import 'package:flutter_state_example/provider/form_provider.dart';
+import 'package:flutter_state_example/provider/provider_state_screen.dart';
+import 'package:flutter_state_example/riverpod/riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:provider/provider.dart' as provider;
 
-import 'riverpod/riverpod_screen.dart';
+enum StateSystem { form, cubit, provider, riverPod }
 
 void main() {
-  // Form State && Cubit
-  //runApp(const MyApp());
+  StateSystem stateSystem = StateSystem.form;
 
-  // Provider
-  // runApp(ChangeNotifierProvider(
-  //     create: (_) => FormProvider(), child: const MyApp()));
-
-  // RiverPod
-  runApp(
-    const ProviderScope(
-      child: MyApp(),
-    ),
-  );
+  switch (stateSystem) {
+    case StateSystem.form:
+      runApp(App(stateSystem: stateSystem));
+      break;
+    case StateSystem.cubit:
+      runApp(App(stateSystem: stateSystem));
+      break;
+    case StateSystem.riverPod:
+      runApp(
+        ProviderScope(
+          child: App(stateSystem: stateSystem),
+        ),
+      );
+      break;
+    case StateSystem.provider:
+      runApp(provider.ChangeNotifierProvider(
+          create: (_) => FormProvider(), child: App(stateSystem: stateSystem)));
+      break;
+  }
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class App extends StatelessWidget {
+  const App({Key? key, required this.stateSystem}) : super(key: key);
 
-  // This widget is the root of your application.
+  final StateSystem stateSystem;
+
   @override
   Widget build(BuildContext context) {
+    Widget home;
+
+    switch (stateSystem) {
+      case StateSystem.form:
+        home = const FormStateScreen();
+        break;
+      case StateSystem.cubit:
+        home = BlocProvider<FormCubit>(
+          create: (BuildContext context) => FormCubit(),
+          child: const CubitStateScreen(),
+        );
+        break;
+      case StateSystem.provider:
+        home = provider.ChangeNotifierProvider(
+            create: (_) => FormProvider(), child: const ProviderStateScreen());
+        break;
+      case StateSystem.riverPod:
+        home = const RiverPodScreen();
+        break;
+    }
     return MaterialApp(
-      title: 'Flutter State Examples',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-
-      // FormState
-      //home: const FormStateScreen(),
-
-      // Provider
-      // home: ChangeNotifierProvider(
-      //     create: (_) => FormProvider(), child: const ProviderStateScreen()),
-
-      // Cubit
-      // home: BlocProvider<FormCubit>(
-      //   create: (BuildContext context) => FormCubit(),
-      //   child: const CubitStateScreen(),
-      // ));
-
-      home: const RiverPodScreen(),
-    );
+        title: 'Flutter State Examples',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: home);
   }
 }
 
